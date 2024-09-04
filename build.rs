f1e9800cdaa2c94abec8f3e8f86d961af370f0fe
Lucas van Dijk <ldijk@broadinstitute.org>
2024-09-04 11:15:27 -0400

    fix: Blocklist many SIMD types
    This fixes the build of the abPOA bindings. Since the SIMD types are
    now not included in the bindings, we had to make abpoa_simd_matrix_t an
    opaque type.

--- build.rs
+++ build.rs
@@ -5,29 +5,7 @@ use glob::glob;
 
 
 fn main() {
-    let mut builder = cc::Build::new();
-        
-    builder
-        .files(
-            glob("abPOA/src/*.c")
-            .expect("Could not find abPOA source files")
-            .map(|x| x.unwrap())
-        )
-        .include("abPOA/include")
-        .define("USE_SIMDE", None)
-        .define("SIMDE_ENABLE_NATIVE_ALIASES", None)
-        .flag("-Wall")
-        .flag("-Wno-unused-function")
-        .flag("-Wno-misleading-indentation");
-        
-    if cfg!(all(target_os = "macos", target_arch = "aarch64")) {
-        builder.define("__AVX2__", None);
-        builder.flag("-march=armv8-a+simd");
-    }
-    
-    builder.compile("abpoa");
-    
-    let compiler_args = [
+    let mut compiler_args = vec![
         "-Wall",
         "-Wno-unused-function",
         "-Wno-misleading-indentation",
@@ -35,7 +13,27 @@ fn main() {
         "-DSIMDE_ENABLE_NATIVE_ALIASES",
     ];
     
+    if cfg!(all(target_os = "macos", target_arch = "aarch64")) {
+        compiler_args.push("-D__AVX2__");
+        compiler_args.push("-march=armv8-a+simd");
+    }
     
+    let mut builder = cc::Build::new();
+        
+    builder
+        .files(
+            glob("abPOA/src/*.c")
+            .expect("Could not find abPOA source files")
+            .map(|x| x.unwrap())
+            .filter(|x| !x.ends_with("abpoa.c"))
+        )
+        .include("abPOA/include");
+    
+    for flag in &compiler_args {
+        builder.flag(flag);
+    }
+    
+    builder.compile("abpoa");
     
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -45,6 +43,14 @@ fn main() {
         // bindings for.
         .header("abPOA/include/abpoa.h")
         .clang_args(&compiler_args)
+        .opaque_type("abpoa_t")
+        .opaque_type("abpoa_simd_matrix_t")
+        .blocklist_type(r".*\d+x\d+_t")
+        .blocklist_type(r".*\d+x\d+x\d+_t")
+        .blocklist_type(r"neon_.*")
+        .blocklist_type(r"simde_.*")
+        .blocklist_type(r"__m\d+[dfi]?")
+        .blocklist_type(r"SIMD[if]")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
