0a2b48d9b6f8e3c6ca2cb8ac6ad3549d050bdde4
Lucas van Dijk <ldijk@broadinstitute.org>
2024-09-04 11:16:58 -0400

    fix: Disable many style warnings for ffi module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use std::ffi::c_void;
 
 mod ffi {
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(dead_code)]
+    
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
