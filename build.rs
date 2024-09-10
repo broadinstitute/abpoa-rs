63ad3d10623b997e5288ebe78f99e1e916fea8b8
Lucas van Dijk <ldijk@broadinstitute.org>
2024-09-10 11:34:32 -0400

    enh: Improve build script organization

--- build.rs
+++ build.rs
@@ -16,17 +16,28 @@ fn main() {
     if cfg!(all(target_os = "macos", target_arch = "aarch64")) {
         compiler_args.push("-D__AVX2__");
         compiler_args.push("-march=armv8-a+simd");
+        compiler_args.push("-mmacosx-version-min=14.0");
     }
-
+    
+    let abpoa_header_files: Vec<_> = glob("abPOA/{src,include}/*.h")
+        .expect("Could not find abPOA header files")
+        .map(|x| x.unwrap())
+        .collect();
+    
+    let abpoa_source_files: Vec<_> = glob("abPOA/src/*.c")
+        .expect("Could not find abPOA source files")
+        .map(|x| x.unwrap())
+        .filter(|x| !x.ends_with("abpoa.c")) // exclude the CLI main file (which also includes a main() function)
+        .collect();
+    
+    // Ensure that we rerun the build script if any of the abPOA header or source files change
+    for f in abpoa_header_files.iter().chain(abpoa_source_files.iter()) {
+        println!("cargo::rerun-if-changed={}", f.display());
+    }
+    
     let mut builder = cc::Build::new();
-
     builder
-        .files(
-            glob("abPOA/src/*.c")
-                .expect("Could not find abPOA source files")
-                .map(|x| x.unwrap())
-                .filter(|x| !x.ends_with("abpoa.c")) // exclude the CLI main file (which also includes a main() function)
-        )
+        .files(&abpoa_source_files)
         .include("abPOA/include");
 
     for flag in &compiler_args {
@@ -34,6 +45,8 @@ fn main() {
     }
 
     builder.compile("abpoa");
+    
+    println!("cargo::rustc-link-lib=z");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -44,6 +57,7 @@ fn main() {
         .header("abPOA/include/abpoa.h")
         .header("abPOA/src/abpoa_seq.h")
         .clang_args(&compiler_args)
+        // Hide SIMD(e) aliases and types, don't work well with bindgen
         .opaque_type("abpoa_simd_matrix_t")
         .blocklist_type(r".*\d+x\d+_t")
         .blocklist_type(r".*\d+x\d+x\d+_t")
